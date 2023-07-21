15fbcf58314bb313819c3f1ddb69f451701731a0
Leandro Bertolami <lbertolami@gmail.com>
2023-07-21 17:30:18 -0600

    Banner for donations

---
+++ components/features/banner/default.jsx
@@ -0,0 +1,52 @@
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import "./styles.scss";
+
+const Banner = ({ customFields }) => {
+  const { title, url, description } = customFields;
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    const bannerFlag = sessionStorage.getItem("banner");
+    if (!bannerFlag) {
+      setShowComponent(true);
+    }
+  }, []);
+
+  const handleClick = () => {
+    sessionStorage.setItem("banner", "hidden");
+  };
+
+  return (
+    <div className="banner">
+      {showComponent && (
+        <div className="Banner-content">
+          <span className="Banner-title">
+            <b>{title}</b>
+          </span>
+
+          <a className="Banner-link" href={url}>
+            {description}
+          </a>
+
+          <button className="Banner-close" onClick={handleClick}>
+            <span>X</span>
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Banner.label = "Banner - Civic";
+Banner.description = "Shows banner below the header";
+
+Banner.propTypes = {
+  customFields: PropTypes.shape({
+    title: PropTypes.string,
+    url: PropTypes.string,
+    description: PropTypes.string,
+  }),
+};
+
+export default Banner;
