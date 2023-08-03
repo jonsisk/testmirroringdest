a7d41f76ee519081b2688fe4548f3ebd75c71535
Leandro Bertolami <lbertolami@gmail.com>
2023-08-03 09:59:02 -0300

    sticky header WIP

---
+++ components/hooks/use-sticky.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+import { useBrowserGlobals } from "./use-browserglobals";
+
+const useSticky = () => {
+  const global = useBrowserGlobals();
+  const stickyRef = useRef(null);
+  const [sticky, setSticky] = useState(false);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    if (!stickyRef.current) {
+      return;
+    }
+
+    setOffset(stickyRef.current.offsetTop);
+  }, [stickyRef, setOffset]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!stickyRef.current) {
+        return;
+      }
+
+      setSticky(global.window.scrollY > offset);
+    };
+    global.window.addEventListener("scroll", handleScroll);
+    return () => global.window.removeEventListener("scroll", handleScroll);
+  }, [setSticky, stickyRef, offset]);
+
+  return { stickyRef, sticky };
+};
+
+export default useSticky;
