00ce2c03ed3cce01b67cf3d4e0f0a942b1b073d7
Leandro Bertolami <lbertolami@gmail.com>
2023-08-10 12:32:41 -0300

    overlay WIP

--- components/layouts/OneColLayout/default.jsx
+++ components/layouts/OneColLayout/default.jsx
@@ -2,7 +2,7 @@ import { useFusionContext } from "fusion:context";
 import React from "react";
 
 const OneColLayout = ({ children }) => {
-  const [navigation, overlay, fullWidth, footer] = children;
+  const [navigation, overlay, overlayText, fullWidth, footer] = children;
   const { arcSite, contextPath, deployment } = useFusionContext();
   const theme = arcSite.split("-")[0];
 
@@ -14,7 +14,8 @@ const OneColLayout = ({ children }) => {
       />
       <div className={`site-${arcSite} custom-layout oneColumn`}>
         <header className="header">{navigation}</header>
-        <div className="overlay">{overlay}</div>
+        <div className="overlay-image">{overlay}</div>
+        <div className="overlay-text">{overlayText}</div>
         <section role="main" id="main" className="main" tabIndex="-1">
           <div className="container layout-section">
             <div className="row-">
@@ -28,6 +29,6 @@ const OneColLayout = ({ children }) => {
   );
 };
 
-OneColLayout.sections = ["navigation", "overlay", "fullwidth", "footer"];
+OneColLayout.sections = ["navigation", "overlay", "overlayText", "fullwidth", "footer"];
 OneColLayout.label = "One Column - Civic Layout";
 export default OneColLayout;
