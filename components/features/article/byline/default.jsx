7dea01009e85ad1dd1166974f1af67ff3a60e549
Leandro Bertolami <lbertolami@gmail.com>
2023-08-16 13:02:05 -0300

    Refactor byline

--- components/features/article/byline/default.jsx
+++ components/features/article/byline/default.jsx
@@ -1,31 +1,14 @@
 import { useFusionContext } from "fusion:context";
 import React from "react";
-import { getUserDate } from "../../../helpers/date.helper";
+import Byline from "../../../base/byline/byline.component";
 
 /**
  * Displays author name and date for the article
  */
 const BylineFeature = () => {
   const { globalContent } = useFusionContext();
-  const credits = globalContent?.credits?.by;
-  const { display_date: displayDate, publish_date: publishDate } = globalContent;
 
-  return (
-    <div className="byline">
-      <span className="author">
-        By&nbsp;
-        {credits.map((author) => {
-          return (
-            <a key={author.slug} href={`/author/${author.slug}`}>
-              {author.name}
-            </a>
-          );
-        })}
-      </span>
-      <span className="separator">&nbsp;|&nbsp;</span>
-      <span className="date">{getUserDate(displayDate ? displayDate : publishDate, true)}</span>
-    </div>
-  );
+  return <Byline element={globalContent} />;
 };
 
 BylineFeature.label = "Byline - Civic";
