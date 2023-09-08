3643eed5caa4ec2cb982a572f0628ebc915c33a5
Leandro Bertolami <lbertolami@gmail.com>
2023-09-08 15:43:17 -0300

    improve meta tags to include rss feeds on home and section pages

--- components/base/metatags/metatags.components.jsx
+++ components/base/metatags/metatags.components.jsx
@@ -2,25 +2,54 @@ import React from "react";
 import { getArticleParselyTags } from "../../helpers/article.helper";
 import { getDateForMetadata } from "../../helpers/date.helper";
 
-const CivicMetaTags = ({ content, parselyTags, arcSite, websiteUrl }) => {
-  if (content?.type !== "story") return null;
+const CivicMetaTags = ({ content, parselyTags, arcSite, websiteUrl, websiteName, pageType }) => {
+  switch (pageType) {
+    case "article":
+      return (
+        <>
+          <meta name="og:image:width" content="1200" />
+          <meta name="og:image:height" content="630" />
+          <meta name="og:image:type" content="image/jpeg" />
+          <meta
+            name="parsely-tags"
+            content={getArticleParselyTags(content, parselyTags, arcSite)}
+          />
 
-  return (
-    <>
-      <meta name="og:image:width" content="1200" />
-      <meta name="og:image:height" content="630" />
-      <meta name="og:image:type" content="image/jpeg" />
-      <meta name="parsely-tags" content={getArticleParselyTags(content, parselyTags, arcSite)} />
+          {content?.credits?.by?.length == 1 && (
+            <meta
+              name="article:author"
+              content={`${websiteUrl}/authors/${content?.credits?.by?.[0]?.slug}`}
+            />
+          )}
+          <meta name="article:published_time" content={getDateForMetadata(content?.display_date)} />
+        </>
+      );
 
-      {content?.credits?.by?.length == 1 && (
-        <meta
-          name="article:author"
-          content={`${websiteUrl}/authors/${content?.credits?.by?.[0]?.slug}`}
-        />
-      )}
-      <meta name="article:published_time" content={getDateForMetadata(content?.display_date)} />
-    </>
-  );
+    case "section":
+      return (
+        <>
+          <link
+            type="application/rss+xml"
+            rel="alternate"
+            title={`${content.name} - ${websiteName}`}
+            href={`${websiteUrl}/arc/outboundfeeds/rss/category${content._id}/`}
+          ></link>
+        </>
+      );
+    case "homepage":
+      return (
+        <>
+          <link
+            type="application/rss+xml"
+            rel="alternate"
+            title={websiteName}
+            href={`${websiteUrl}/arc/outboundfeeds/rss/`}
+          ></link>
+        </>
+      );
+    default:
+      return null;
+  }
 };
 
 export default CivicMetaTags;
