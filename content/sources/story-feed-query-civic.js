ca563b8b9a1daaf18685ae3cf1139bbf3bcb2f58
Leandro Bertolami <lbertolami@gmail.com>
2023-10-04 11:21:06 -0300

    custom content sources to support new resize options

---
+++ content/sources/story-feed-query-civic.js
@@ -0,0 +1,16 @@
+import getResizedImageData from "../../components/helpers/image.helper";
+
+export default {
+  resolve: (params) =>
+    `/content/v4/search/published?q=${params.query || "*"}&website=${params["arc-site"]}&size=${
+      params.size || 8
+    }&from=${params.offset || 0}&sort=display_date:desc`,
+  schemaName: "ans-feed",
+  params: {
+    query: "text",
+    size: "number",
+    offset: "number",
+  },
+  // other options null use default functionality, such as filter quality
+  transform: (data, query) => getResizedImageData(data, null, null, null, query["arc-site"]),
+};
