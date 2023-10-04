2d1ddc124297b7f4327ca039dc103b56e6712218
Feiner Valencia <feinvalencia@gmail.com>
2023-10-04 09:34:27 -0500

    add styles card in stories

--- components/features/storiesMissed/storiesCard.jsx
+++ components/features/storiesMissed/storiesCard.jsx
@@ -1,6 +1,6 @@
 import { Image } from "@wpmedia/engine-theme-sdk";
 import { extractResizedParams, extractImageFromStory } from "@wpmedia/resizer-image-block";
-import { Overline, Byline, Heading, SecondaryFont } from "@wpmedia/shared-styles";
+import { Heading, SecondaryFont } from "@wpmedia/shared-styles";
 import getProperties from "fusion:properties";
 import React from "react";
 
@@ -24,7 +24,6 @@ export const StoriesCard = ({
     resizerURL,
   };
   const imageURL = extractImageFromStory(slide);
-
   return (
     <div className="slide article-slide">
       {showImage && (
@@ -38,10 +37,14 @@ export const StoriesCard = ({
           fallbackImage={targetFallbackImage}
         />
       )}
-      <div className="article-slide-info">
-        <Overline story={slide} className="overline" />
+      <div
+        className="article-slide-info"
+        style={{ borderBottom: "1px solid #ccc", paddingBottom: "1rem" }}
+      >
         <a href={slide.canonical_url} target="_blank" rel="noreferrer">
-          <Heading className="headline-text">{slide.headlines.basic}</Heading>
+          <Heading className="headline-text" style={{ margin: 0 }}>
+            {slide.headlines.basic}
+          </Heading>
         </a>
         <div className="results-list--description-author-container">
           <a href={slide.canonical_url} title={slide.headlines.basic}>
@@ -49,9 +52,6 @@ export const StoriesCard = ({
               {slide.subheadlines?.basic}
             </SecondaryFont>
           </a>
-          <div className="results-list--author-date">
-            <Byline content={slide} list font="Primary" />
-          </div>
         </div>
       </div>
     </div>
