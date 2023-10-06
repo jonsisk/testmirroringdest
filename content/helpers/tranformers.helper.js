10704981875918912c20d63e2ce5c6d6eb185534
Leandro Bertolami <lbertolami@gmail.com>
2023-10-06 11:48:15 -0300

    remove old funcion / add addPath helper for sources

--- content/helpers/tranformers.helper.js
+++ content/helpers/tranformers.helper.js
@@ -1,25 +1,10 @@
 import getProperties from "fusion:properties";
-import getResizedImageData from "../../components/helpers/image.helper";
 
-export const transformFeedData = (data, query) => {
-  const transformedData = getResizedImageData(data, null, null, null, query["arc-site"]);
-  if (!query.keepPrimaryWebsite) {
-    return transformedData;
+// adds correct ad-path to data depending if bureau section is present or not
+export const addAdPath = (content, arcSite) => {
+  if (content["site_section"]) {
+    content["adpath"] = content["site_section"]?._id?.replace(/^\//, "");
+  } else {
+    content["adpath"] = getProperties(arcSite)?.gamSiteId;
   }
-  transformedData.content_elements = transformedData.content_elements.map((item) => {
-    const primaryWebsite = item.canonical_website;
-    if (primaryWebsite) {
-      // let's adjust the URL to the primary website
-      const { websiteDomain } = getProperties(primaryWebsite);
-      const canonicalUrl = item.canonical_url;
-      item.website_url = `${websiteDomain}${canonicalUrl}`;
-      item.canonical_url = `${websiteDomain}${canonicalUrl}`;
-
-      for (const key in item.websites) {
-        item.websites[key].website_url = `${websiteDomain}${canonicalUrl}`;
-      }
-    }
-    return item;
-  });
-  return transformedData;
 };
