aadcb24430698fde3ec7b4136e459e8be04ffe45
Leandro Bertolami <lbertolami@gmail.com>
2023-10-19 19:30:23 -0300

    Refactor byline to handle multiple authors

--- components/base/byline/byline.component.jsx
+++ components/base/byline/byline.component.jsx
@@ -1,40 +1,66 @@
 /* eslint-disable react/no-unknown-property */
 import React from "react";
 import { getUserDate, isDateAfter } from "../../helpers/date.helper";
+import Author from "./author.component";
 
 const Byline = ({ element, showTime = true, showDate = true, websiteDomain, showBorder }) => {
   const credits = element?.credits?.by;
 
-  const { display_date: displayDate, publish_date: publishDate } = element;
+  const { display_date: displayDate, publish_date: publishDate, type } = element;
+
+  //Render all authors for a story
+  const renderAuthors = (authorsList) => {
+    return authorsList.map((author, index) => {
+      if (index === 0) {
+        return <Author key={index} author={author} websiteDomain={websiteDomain} />;
+      } else if (index === authorsList.length - 1) {
+        return (
+          <>
+            <span className="and">and</span>
+            <Author key={index} author={author} websiteDomain={websiteDomain} />
+          </>
+        );
+      } else {
+        return (
+          <>
+            <span>,</span>
+            <Author key={index} author={author} websiteDomain={websiteDomain} />
+          </>
+        );
+      }
+    });
+  };
 
   return (
     <div className={showBorder === false ? "bylineNotice" : "byline"}>
       <span className="author">
         By&nbsp;
-        {credits?.map((author) => {
-          return (
-            <div key={author.slug} className="Page-byline">
-              <div className="Page-authors">
-                {credits.length > 3 &&
-                credits[0].name !== author.name &&
-                credits[1].name !== author.name &&
-                credits[2].name !== author.name
-                  ? " and " + credits.length - 3 + " more "
-                  : credits[0].name !== author.name &&
-                    credits[credits.length - 1].name !== author.name
-                  ? ", "
-                  : credits[credits.length - 1].name === author.name && credits.length > 1
-                  ? " and "
-                  : ""}
-                {author.slug ? (
-                  <a href={`${websiteDomain}/authors/${author.slug}`}>{author.name}</a>
-                ) : (
-                  author.name
-                )}
-              </div>
-            </div>
-          );
-        })}
+        {credits.length === 1 && <Author author={credits[0]} websiteDomain={websiteDomain} />}
+        {credits.length === 2 && (
+          <>
+            <Author author={credits[0]} websiteDomain={websiteDomain} />
+            <span className="and">and</span>
+            <Author author={credits[1]} websiteDomain={websiteDomain} />
+          </>
+        )}
+        {credits.length === 3 && (
+          <>
+            <Author author={credits[0]} websiteDomain={websiteDomain} /> <span>,</span>
+            <Author author={credits[1]} websiteDomain={websiteDomain} />
+            <span className="and">and</span>
+            <Author author={credits[2]} websiteDomain={websiteDomain} />
+          </>
+        )}
+        {credits.length >= 4 &&
+          (type === "story" ? (
+            renderAuthors(credits)
+          ) : (
+            <>
+              <Author author={credits[0]} websiteDomain={websiteDomain} /> <span>,</span>
+              <Author author={credits[1]} websiteDomain={websiteDomain} />
+              <span className="and">{` and ${credits.length - 2} more`}</span>
+            </>
+          ))}
       </span>
       {showDate && displayDate && <span className="separator">&nbsp;|&nbsp;</span>}
       {showDate && displayDate && (
