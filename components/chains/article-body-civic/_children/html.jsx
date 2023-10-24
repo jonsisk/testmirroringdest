0697334fbd6871fb108ff2c220549a9a26c6456a
Feiner Valencia <feinvalencia@gmail.com>
2023-10-24 08:14:24 -0500

    add tableparser function and lb react table

--- components/chains/article-body-civic/_children/html.jsx
+++ components/chains/article-body-civic/_children/html.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Static from "fusion:static";
 import styled from "styled-components";
+import { extractTableData } from "../../../../content/helpers/tableParser.helper";
 
 const StyledDiv = styled.div`
-	a {
-		color: ${(props) => props.primaryColor};
-	}
+  a {
+    color: ${(props) => props.primaryColor};
+  }
 `;
 
-const HTML = ({ id, content, primaryColor }) =>
-	!content ? null : (
-		<Static id={`article-html-block-${id}`}>
-			<StyledDiv
-				className="block-margin-bottom"
-				dangerouslySetInnerHTML={{ __html: content }}
-				primaryColor={primaryColor}
-			/>
-		</Static>
-	);
+const HTML = ({ id, content, primaryColor }) => {
+  console.log(extractTableData(content), "content-----");
+  return !content ? null : (
+    <Static id={`article-html-block-${id}`}>
+      <StyledDiv
+        className="block-margin-bottom"
+        dangerouslySetInnerHTML={{ __html: content }}
+        primaryColor={primaryColor}
+      />
+    </Static>
+  );
+};
 
 export default HTML;
---
+++ content/helpers/tableParser.helper.js
@@ -0,0 +1,22 @@
+export function extractTableData(htmlString) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, "text/html");
+
+  // Extract column headers
+  const headers = Array.from(doc.querySelectorAll("figure table thead tr th")).map((th) => ({
+    Header: th.textContent.trim(),
+    accessor: th.textContent.trim().toLowerCase(),
+  }));
+
+  // Extract row data
+  const rows = Array.from(doc.querySelectorAll("figure table tbody tr")).map((tr) => {
+    const cells = tr.querySelectorAll("td");
+    const rowData = {};
+    cells.forEach((cell, index) => {
+      rowData[headers[index].accessor] = cell.textContent.trim();
+    });
+    return rowData;
+  });
+
+  return { headers, rows };
+}
