416ad52e60d388b846837d8dd625747cfbbdc45c
Feiner Valencia <feinvalencia@gmail.com>
2023-10-30 13:55:10 -0500

    fix dataTable

--- components/chains/article-body-civic/_children/html.jsx
+++ components/chains/article-body-civic/_children/html.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import Static from "fusion:static";
-import styled from "styled-components";
+import DataTable from "./dataTable";
+import { extractTableData } from "../../../../content/helpers/tableParser.helper";
 
-const StyledDiv = styled.div`
-  a {
-    color: ${(props) => props.primaryColor};
-  }
-`;
+const HTML = ({ content, id }) => {
+  // busca si el contenido tiene una tabla
+  const isTableContent =
+    content.includes("<table>") && content.includes("<thead>") && content.includes("<tbody>");
 
-const HTML = ({ id, content, primaryColor }) =>
-  !content ? null : (
-    <Static id={`article-html-block-${id}`}>
-      <StyledDiv
-        className="block-margin-bottom"
-        dangerouslySetInnerHTML={{ __html: content }}
-        primaryColor={primaryColor}
-      />
-    </Static>
+  // Extraer los datos si el contenido es una tabla
+  const tableData = isTableContent ? extractTableData(content) : null;
+
+  return (
+    <div id={`article-html-block-${id}`}>
+      {isTableContent ? (
+        <DataTable columns={tableData.headers} data={tableData.rows} />
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      )}
+    </div>
   );
+};
 
 export default HTML;
