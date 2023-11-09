be0e1b0bf8dbe8d9152d6e1038535516b2b10c16
Leandro Bertolami <lbertolami@gmail.com>
2023-11-09 11:42:49 -0300

    section images

--- components/features/section-title/_children/section-title.jsx
+++ components/features/section-title/_children/section-title.jsx
@@ -1,8 +1,28 @@
 import { PrimaryFont } from "@wpmedia/shared-styles";
+import { useFusionContext } from "fusion:context";
 import React from "react";
 
 const SectionTitleCivic = (props) => {
   const { content } = props;
+  const context = useFusionContext();
+  const { deployment, contextPath } = context;
+
+  if (content?.sidebar && content.sidebar.sidebar_logo) {
+    return (
+      <>
+        <div className="section-logo">
+          <img
+            src={
+              content?.sidebar.sidebar_logo.startsWith("/")
+                ? deployment(`${contextPath}/resources/images${content.sidebar.sidebar_logo}`)
+                : content.sidebar.sidebar_logo
+            }
+          />
+        </div>
+        <h2 className="section-tagline">{content.site?.site_tagline}</h2>
+      </>
+    );
+  }
 
   return (
     !!(content && (content.name || content.display_name)) && (
