d269eba32a45ba9a4659bc408eeaaa6a75b3e75e
Leandro Bertolami <lbertolami@gmail.com>
2023-11-28 17:32:36 -0300

    make redirect work on composer preview

--- content/sources/content-api-civic-composer-preview.js
+++ content/sources/content-api-civic-composer-preview.js
@@ -3,6 +3,13 @@ import { CONTENT_BASE, ARC_ACCESS_TOKEN } from "fusion:environment";
 import getResizedImageData from "../../components/helpers/image.helper";
 import { addAdPath } from "../helpers/tranformers.helper";
 
+const RedirectError = (location, message = "Redirect", code = 302) => {
+  const err = new Error(message);
+  err.statusCode = code;
+  err.location = location;
+  return err;
+};
+
 const fetch = async ({ _id, section, "arc-site": arcSite }) => {
   // get the article
   const { data: articleData } = await axios({
@@ -13,6 +20,14 @@ const fetch = async ({ _id, section, "arc-site": arcSite }) => {
       Authorization: `Bearer ${ARC_ACCESS_TOKEN}`,
     },
   });
+
+  // check if article has redirect and do a redirect
+  if (articleData?.related_content?.redirect) {
+    const redirectUrl = articleData?.related_content?.redirect[0]?.redirect_url;
+    if (redirectUrl) {
+      throw new RedirectError(redirectUrl);
+    }
+  }
 
   // section comes in the form of vb-arizona or vb-national
   // we convert it to a section meaning
